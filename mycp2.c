ffd94ef7d1273b06cc0794b014915b9bfbbc314e
i18suetugu <82075653+i18suetugu@users.noreply.github.com>
2021-04-22 19:20:22 +0900

    Update mycp2.c

--- mycp2.c
+++ mycp2.c
@@ -5,16 +5,50 @@
 #define  BSIZ 1000                 // バッファサイズ
 
 int main(int argc, char *argv[]) {
-  int fd1;                         // コピー元用のFD
-  int fd2;                         // コピー先用のFD
+  int fd1;                         // コピー元用のFD(ファイルディレクトリ)
+  int fd2;                         // コピー先用のFD(ファイルディレクトリ)
   ssize_t len;                     // 実際に読んだバイト数
   char buf[BSIZ];                  // バッファ
+  
+  //openシステムコール
 
-  // 以下にプログラムを書き加えて完成させる．
-  //
-  // ヒント：書き込みopenは次のようにすると良い．
-  // fd2 = open(argv[2], O_WRONLY|O_TRUNC|O_CREAT, 0644);
-
+  fd1 = open("argv[1], O_RDONLY);        //コピー元用のFDを読み出し用にオープン
+  fd2 = open("argv[2], O_WRONLY, 0644);  //コピー先用のFDを書き出し用にオープン
+             
+  if(fd1<0){                       //openシステムコールのエラーチェック
+    perror("argv[1]");　　　　　　　//argv[1]にエラー原因を表示
+    exit(1);                       //エラー表示
+  }
+  
+  if(fd2<0){                       //openシステムコールのエラーチェック
+    perror("argv[2]");             //argv[2]にエラー原因を表示
+    exit(1);                       //エラー表示
+  }
+  
+  //readシステムコール
+             
+  int n=0;                         //int型の変数nを宣言し0を代入する
+  while ((n = read(fd1, buf, 100)) > 0){  
+       //readシステムコールでファイルの先頭から順番にデータを読み出す
+    ssize_t len = ssize_t len + n;         //現在時点のバイト数を表示
+  }
+             
+  if(fd1<0){                       //readシステムコールのエラーチェック
+    perror("argv[1]");　　　　　　　//argv[1]にエラー原因を表示
+    exit(1);                       //エラー表示
+  }
+             
+  //writeシステムコール
+             
+  while ((n = write(fd2, buf, ssize_t len)) > 0){
+       //writeシステムコールでファイルの先頭から順番にデータを書き込む
+  }
+  
+  if(fd2<0){                       //writeシステムコールのエラーチェック
+    perror("argv[2]");             //argv[2]にエラー原因を表示
+    exit(1);                       //エラー表示
+  }
+  
   close(fd1);
   close(fd2);
   return 0;                        // 正常終了
